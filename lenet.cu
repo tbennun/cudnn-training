0f6b7d18b7307f2ac0b32f7d5b784b9182e37df2
3DdeepAI <28405357+3DdeepAI@users.noreply.github.com>
2018-08-25 20:19:38 +0200

    moved layer structs to NNlayers.h

--- lenet.cu
+++ lenet.cu
@@ -46,20 +46,14 @@
 // Block width for CUDA kernels
 #define BW 128
 
-#ifdef USE_GFLAGS
-    #include <gflags/gflags.h>
 
-    #ifndef _WIN32
-        #define gflags google
-    #endif
-#else
     // Constant versions of gflags
     #define DEFINE_int32(flag, default_value, description) const int FLAGS_##flag = (default_value)
     #define DEFINE_uint64(flag, default_value, description) const unsigned long long FLAGS_##flag = (default_value)
     #define DEFINE_bool(flag, default_value, description) const bool FLAGS_##flag = (default_value)
     #define DEFINE_double(flag, default_value, description) const double FLAGS_##flag = (default_value)
     #define DEFINE_string(flag, default_value, description) const std::string FLAGS_##flag ((default_value))
-#endif
+
 
 /**
  * Computes ceil(x / y) for integral nonnegative values.
@@ -156,160 +150,7 @@ DEFINE_double(lr_power, 0.75, "Learning rate policy power");
 ///////////////////////////////////////////////////////////////////////////////////////////
 // Layer representations
 
-/**
- * Represents a convolutional layer with bias.
- */
-struct ConvBiasLayer
-{
-    int in_channels, out_channels, kernel_size;
-    int in_width, in_height, out_width, out_height;
-
-    std::vector<float> pconv, pbias;
-    
-    ConvBiasLayer(int in_channels_, int out_channels_, int kernel_size_, 
-                  int in_w_, int in_h_) : pconv(in_channels_ * kernel_size_ * kernel_size_ * out_channels_), 
-                  pbias(out_channels_)
-    {
-        in_channels = in_channels_;
-        out_channels = out_channels_;
-        kernel_size = kernel_size_;
-        in_width = in_w_;
-        in_height = in_h_;
-        out_width = in_w_ - kernel_size_ + 1;
-        out_height = in_h_ - kernel_size_ + 1;
-    }
-
-    bool FromFile(const char *fileprefix)
-    {
-        std::stringstream ssf, ssbf;
-        ssf << fileprefix << ".bin";
-        ssbf << fileprefix << ".bias.bin";
-        
-        // Read weights file
-        FILE *fp = fopen(ssf.str().c_str(), "rb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
-            return false;
-        }
-        fread(&pconv[0], sizeof(float), in_channels * out_channels * kernel_size * kernel_size, fp);
-        fclose(fp);
-
-        // Read bias file
-        fp = fopen(ssbf.str().c_str(), "rb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
-            return false;
-        }
-        fread(&pbias[0], sizeof(float), out_channels, fp);
-        fclose(fp);
-        return true;
-    }
-
-    void ToFile(const char *fileprefix)
-    {
-        std::stringstream ssf, ssbf;
-        ssf << fileprefix << ".bin";
-        ssbf << fileprefix << ".bias.bin";
-
-        // Write weights file
-        FILE *fp = fopen(ssf.str().c_str(), "wb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
-            exit(2);
-        }
-        fwrite(&pconv[0], sizeof(float), in_channels * out_channels * kernel_size * kernel_size, fp);
-        fclose(fp);
-
-        // Write bias file
-        fp = fopen(ssbf.str().c_str(), "wb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
-            exit(2);
-        }
-        fwrite(&pbias[0], sizeof(float), out_channels, fp);
-        fclose(fp);
-    }
-};
-
-/**
- * Represents a max-pooling layer.
- */
-struct MaxPoolLayer
-{
-    int size, stride;
-    MaxPoolLayer(int size_, int stride_) : size(size_), stride(stride_) {}
-};
-
-/**
- * Represents a fully-connected neural network layer with bias.
- */
-struct FullyConnectedLayer
-{
-    int inputs, outputs;
-    std::vector<float> pneurons, pbias;
-
-    FullyConnectedLayer(int inputs_, int outputs_) : outputs(outputs_), inputs(inputs_),
-        pneurons(inputs_ * outputs_), pbias(outputs_) {}
-
-    bool FromFile(const char *fileprefix)
-    {
-        std::stringstream ssf, ssbf;
-        ssf << fileprefix << ".bin";
-        ssbf << fileprefix << ".bias.bin";
-
-        // Read weights file
-        FILE *fp = fopen(ssf.str().c_str(), "rb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
-            return false;
-        }
-        fread(&pneurons[0], sizeof(float), inputs * outputs, fp);
-        fclose(fp);
-
-        // Read bias file
-        fp = fopen(ssbf.str().c_str(), "rb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
-            return false;
-        }
-        fread(&pbias[0], sizeof(float), outputs, fp);
-        fclose(fp);
-        return true;
-    }
-
-    void ToFile(const char *fileprefix)
-    {
-        std::stringstream ssf, ssbf;
-        ssf << fileprefix << ".bin";
-        ssbf << fileprefix << ".bias.bin";
-
-        // Write weights file
-        FILE *fp = fopen(ssf.str().c_str(), "wb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
-            exit(2);
-        }
-        fwrite(&pneurons[0], sizeof(float), inputs * outputs, fp);
-        fclose(fp);
-
-        // Write bias file
-        fp = fopen(ssbf.str().c_str(), "wb");
-        if (!fp)
-        {
-            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
-            exit(2);
-        }
-        fwrite(&pbias[0], sizeof(float), outputs, fp);
-        fclose(fp);
-    }
-};
+#include "NNlayers.h"
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
