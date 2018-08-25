9948839d4e37f1ffe1212bbc831f95cec28c0978
3DdeepAI <28405357+3DdeepAI@users.noreply.github.com>
2018-08-25 20:17:48 +0200

    Create NNlayers.h

---
+++ NNlayers.h
@@ -0,0 +1,168 @@
+#pragma once
+
+// Disclaimer: No Warranty. Use at your own risk.  (See License Information in Root)
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Layer representations
+
+
+// from https://github.com/tbennun/cudnn-training
+// Represents a convolutional layer with bias.
+struct ConvBiasLayer
+{
+    int in_channels, out_channels, kernel_size;
+    int in_width, in_height, out_width, out_height;
+
+    std::vector<float> pneurons, pbias;
+
+    
+    ConvBiasLayer(int in_channels_, int out_channels_, int kernel_size_,  int in_w_, int in_h_) : 
+                  pneurons(in_channels_ * kernel_size_ * kernel_size_ * out_channels_),  pbias(out_channels_)
+    {
+        in_channels = in_channels_;
+        out_channels = out_channels_;
+        kernel_size = kernel_size_;
+        in_width = in_w_;
+        in_height = in_h_;
+        out_width = in_w_ - kernel_size_ + 1;
+        out_height = in_h_ - kernel_size_ + 1;
+    }
+
+    bool FromFile(const char *fileprefix)
+    {
+        std::stringstream ssf, ssbf;
+        ssf << fileprefix << ".bin";
+        ssbf << fileprefix << ".bias.bin";
+        
+        // Read weights file
+        FILE *fp = fopen(ssf.str().c_str(), "rb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
+            return false;
+        }
+        fread(&pneurons[0], sizeof(float), in_channels * out_channels * kernel_size * kernel_size, fp);
+        fclose(fp);
+
+        // Read bias file
+        fp = fopen(ssbf.str().c_str(), "rb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
+            return false;
+        }
+        fread(&pbias[0], sizeof(float), out_channels, fp);
+        fclose(fp);
+        return true;
+    }
+
+    void ToFile(const char *fileprefix)
+    {
+        std::stringstream ssf, ssbf;
+        ssf << fileprefix << ".bin";
+        ssbf << fileprefix << ".bias.bin";
+
+        // Write weights file
+        FILE *fp = fopen(ssf.str().c_str(), "wb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
+            exit(2);
+        }
+        fwrite(&pneurons[0], sizeof(float), in_channels * out_channels * kernel_size * kernel_size, fp);
+        fclose(fp);
+
+        // Write bias file
+        fp = fopen(ssbf.str().c_str(), "wb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
+            exit(2);
+        }
+        fwrite(&pbias[0], sizeof(float), out_channels, fp);
+        fclose(fp);
+    }
+};
+
+
+
+// from https://github.com/tbennun/cudnn-training
+// Represents a max-pooling layer.
+struct MaxPoolLayer
+{
+    int size, stride;
+    MaxPoolLayer(int size_, int stride_) : size(size_), stride(stride_) {}
+};
+
+
+
+// from https://github.com/tbennun/cudnn-training
+// Represents a fully-connected neural network layer with bias.
+struct FullyConnectedLayer
+{
+    int inputs, outputs;
+    std::vector<float> pneurons, pbias;
+
+
+    FullyConnectedLayer(int inputs_, int outputs_) : 
+        outputs(outputs_), inputs(inputs_),
+        pneurons(inputs_ * outputs_), pbias(outputs_)
+    {}
+
+    bool FromFile(const char *fileprefix)
+    {
+        // tbennun:  Each file is actually the weights and biases (float32) in binary format, without any headers. 
+        std::stringstream ssf, ssbf;
+        ssf << fileprefix << ".bin";
+        ssbf << fileprefix << ".bias.bin";
+
+        // Read weights file
+        FILE *fp = fopen(ssf.str().c_str(), "rb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
+            return false;
+        }
+        fread(&pneurons[0], sizeof(float), inputs * outputs, fp);
+        fclose(fp);
+
+        // Read bias file
+        fp = fopen(ssbf.str().c_str(), "rb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
+            return false;
+        }
+        fread(&pbias[0], sizeof(float), outputs, fp);
+        fclose(fp);
+        return true;
+    }
+
+    void ToFile(const char *fileprefix)
+    {
+        std::stringstream ssf, ssbf;
+        ssf << fileprefix << ".bin";
+        ssbf << fileprefix << ".bias.bin";
+
+        // Write weights file
+        FILE *fp = fopen(ssf.str().c_str(), "wb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssf.str().c_str());
+            exit(2);
+        }
+        fwrite(&pneurons[0], sizeof(float), inputs * outputs, fp);
+        fclose(fp);
+
+        // Write bias file
+        fp = fopen(ssbf.str().c_str(), "wb");
+        if (!fp)
+        {
+            printf("ERROR: Cannot open file %s\n", ssbf.str().c_str());
+            exit(2);
+        }
+        fwrite(&pbias[0], sizeof(float), outputs, fp);
+        fclose(fp);
+    }
+};
