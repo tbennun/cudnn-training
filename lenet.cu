e08e8e2ecb61452d59757347452f3085950d60b7
3DdeepAI <28405357+3DdeepAI@users.noreply.github.com>
2018-08-25 20:05:51 +0200

    moved GPU kernels to NNkernels.h

--- lenet.cu
+++ lenet.cu
@@ -318,57 +318,13 @@ struct FullyConnectedLayer
     }
 };
 
+
 ///////////////////////////////////////////////////////////////////////////////////////////
 // GPU Kernels
 
-/**
- * Fills a floating-point array with ones.
- *
- * @param vec The array to fill.
- * @param size The number of elements in the array.
- */
-__global__ void FillOnes(float *vec, int size)
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx >= size)
-        return;
-
-    vec[idx] = 1.0f;
-}
+#include "NNkernels.h"
 
 
-
-/**
- * Computes the backpropagation results of the Softmax loss for each result in a batch.
- * Uses the softmax values obtained from forward propagation to compute the difference.
- *
- * @param label The training batch label values.
- * @param num_labels The number of possible labels.
- * @param batch_size The size of the trained batch.
- * @param diff The resulting gradient.
- */
-__global__ void SoftmaxLossBackprop(const float *label, int num_labels, int batch_size, float *diff)
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx >= batch_size)
-        return;
-
-    const int label_value = static_cast<int>(label[idx]);
-
-    // For each item in the batch, decrease the result of the label's value by 1
-    diff[idx * num_labels + label_value] -= 1.0f;
-}
-
-
-
-__global__ void FillZeroes(float *vec, int size)
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx >= size)
-        return;
-
-    vec[idx] = 0.0f;
-}
 
 
 #ifdef USE_NESTEROV_MOMENTUM
