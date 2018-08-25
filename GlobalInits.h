a8066c7343457c1fff60e1983e933e1fa46f513a
3DdeepAI <28405357+3DdeepAI@users.noreply.github.com>
2018-08-25 22:20:33 +0200

    Update GlobalInits.h

--- GlobalInits.h
+++ GlobalInits.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Disclaimer: No Warranty. Use at your own risk.  (See License Information in Root)
+// Disclaimer: No Warranty. Use at your own risk. 
 
 
 // =========================================================================================================
@@ -17,20 +17,20 @@
 
 
 
-#define BATCH_SIZE  64 // 32 // 256 // 4096  // ORG=64
+#define BATCH_SIZE  64
 // BW = Block width for CUDA kernels
-#define defaultBW  (BATCH_SIZE  * 2)      // 128
+#define defaultBW  (BATCH_SIZE  * 2)  // 128
 #define ITERATIONS   1000
 #define LEARNING_RATE 0.001 //  ORG=0.01
-#define LEARNING_RATE_POLICY_GAMMA 0.0001 // 0.00001    // 0.000001 // ORG: 0.0001
-#define LEARNING_RATE_POLICY_POWER   0.85 // 0.8 // 0.31 // 0.51 // 0.2 // 0.69  // 0.75  // ORG: 0.75
+#define LEARNING_RATE_POLICY_GAMMA 0.0001
+#define LEARNING_RATE_POLICY_POWER   0.85 // ORG: 0.75
 // Adam:
-#define BETA1 0.9f     // ORG: 0.9f
-#define BETA2 0.999f // ORG:  0.999f
+#define BETA1 0.9f
+#define BETA2 0.999f
 
 
 
-#define DROP_RATE 0.0 // 0.001 // 0.4      0.0=OFF
+#define DROP_RATE 0.0 // 0.4      0.0=OFF
 #define MOMENTUM 0.9  // 0.0=OFF
 
 
@@ -64,8 +64,8 @@ double FLAGS_momentum = MOMENTUM;
 double FLAGS_drop_rate = DROP_RATE;
 
 bool Adam = false;
-float Beta1 = BETA1; //  0.9f;     // decay term
-float Beta2 = BETA2; // 0.999f; // decay term
+float Beta1 = BETA1;  // decay term
+float Beta2 = BETA2;  // decay term
 
 bool Adamax = false;
 bool Nadam = false;
@@ -81,33 +81,6 @@ struct PolicyStep
   double LearningRate;
 };
 int PolicySteps = 4;   // 0=OFF
-/*
-PolicyStep LearningRatePolicy[MAX_POLICY_STEPS] = 
-{ 
-    { 0, 0.001f  },
-    { 100, 0.0005f  },
-    { 500, 0.0003f  },
-    { 750, 0.0001f  }
-};
-*/
-/*
-PolicyStep LearningRatePolicy[MAX_POLICY_STEPS] = 
-{ 
-    { 0, 0.001f  },
-    { 100, 0.0004f  },
-    { 500, 0.0002f  },
-    { 750, 0.00005f  }
-};
-*/
-/*
-PolicyStep LearningRatePolicy[MAX_POLICY_STEPS] = 
-{ 
-    { 0, 0.001f  },
-    { 100, 0.0005f  },
-    { 400, 0.0003f  },
-    { 750, 0.00007f  }
-};
-*/
 PolicyStep LearningRatePolicy[MAX_POLICY_STEPS] = 
 { 
     { 0, 0.001f  },
@@ -121,14 +94,6 @@ double ExponentialDecayK = 0.001f;
 
 double StepDecayScheduleDrop = 0.56; //  0.0=OFF
 double StepDecayScheduleEpochsDrop = 250.0f;
-
-// Batch parameters
-//DEFINE_uint64(batch_size, 64, "Batch size for training");
-//DEFINE_uint64(batch_size, BATCH_SIZE , "Batch size for training");
-
-    // With compute capability 3.0 or higher, you can have up to 2^32 - 1 blocks in the x-dimension, 
-    // and at most 65535 blocks in the y and z dimensions. See Table H.1. 
-    // Feature Support per Compute Capability of the CUDA C Programming Guide Version 9.1.
 
 DEFINE_bool(pretrained, false, "Use the pretrained CUDNN model as input");
 DEFINE_bool(save_data, false, "Save pretrained weights to file");
